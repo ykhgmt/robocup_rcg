02fd967bcb393567b262c6f7c70db2f15206b125
yuki hagimoto <i2mka5qw@gmail.com>
2014-12-02 00:00:41 +0900

    後方リストを取り出せるように成る

--- rcg_parser.ml
+++ rcg_parser.ml
@@ -10,8 +10,12 @@ module Sexp : sig
 end
 *)
 
-let parsep =
+let s_parse1 =
 "((l 1) 0 0x9 -49 0 0 0 2.327 0 (v h 180) (s 8000 1 1 130600) (c 0 0 906 0 1 907 1 0 0 0 0))";;
+
+let s_parse2 =
+  "(((l 1) 0 0x9 -49 0 0 0 2.327 0 (v h 180) (s 8000 1 1 130600) (c 0 0 906 0 1 907 1 0 0 0 0))
+((l 2) 3 0x1 -25 -5 0 0 78.518 0 (v h 180) (s 8000 0.901147 1 130600) (c 0 0 887 0 1 888 1 0 0 0 0)))";;
 
 type agent =
  Label of int
@@ -19,7 +23,6 @@ type agent =
 | Pos_y of int
 | V_x of int
 | V_y of int
-
 ;;
 
 type agent_inf =
@@ -29,41 +32,45 @@ type agent_inf =
 (* string -> t*)
 let lexing s = scan_sexp (Lexing.from_string s);;
 
-
 let matching a =
   match a with
     List ( List ( Atom "l" :: rest) :: (Atom a) ::  [List(Atom "d" :: rest2)])  -> a
   | _ -> failwith "NO"
 ;;
 
-let matching2 s =
-  let matching2 s =
-    match s with
-      List ( List ( Atom "l" :: [Atom num]) :: b1 :: b2
-             :: (Atom x) :: (Atom y) :: (Atom vx) :: (Atom vy)
-             :: b3 :: b4
-             :: List (Atom "v" :: rest1)
-             :: List (Atom "s" :: rest2)
-             :: List (Atom "c" :: rest3)
-             :: rest4)
-      ->
-        Record (Label (int_of_string num) ,
-                Pos_x (int_of_string x) ,
-                Pos_y (int_of_string y) ,
-                V_x (int_of_string vx) ,
-                V_y (int_of_string vy))
-    | _ -> failwith "NO"
-
-  in
-  let lex = Lexing.from_string s in
-  matching2 (scan_sexp lex)
+(* rest4 == []  *)
+let rec parse s =
+  match s with
+  List
+      (List
+         (List( Atom a :: [Atom num])
+          :: b1 :: b2
+          :: (Atom x) :: (Atom y) :: (Atom vx) :: (Atom vy)
+          :: b3 :: b4
+          :: List (Atom "v" :: rest1)
+          :: List (Atom "s" :: rest2)
+          :: List (Atom "c" :: rest3)
+          :: rest4
+         ) :: rest5)
+    -> (*rest5*)
+      Record (Label (int_of_string num) ,
+              Pos_x (int_of_string x) ,
+              Pos_y (int_of_string y) ,
+              V_x (int_of_string vx) ,
+              V_y (int_of_string vy))
+      ::
+  | _ -> failwith "NO"
 ;;
 
+let parse_lex s =
+  let lex = Lexing.from_string s
+  in parse (scan_sexp lex)
+;;
 
 (* test *)
 (*matching (lexing parsep);;*)
 matching (lexing "((l a) a  (d e))");;
-matching2 parsep;;
+parse_lex s_parse2;;
 
 (* string -> rcg *)
 (*
