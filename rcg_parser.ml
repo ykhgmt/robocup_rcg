011725e4e57a839d528c6d1b2fa26d3b5bb1f974
yuki hagimoto <i2mka5qw@gmail.com>
2014-12-01 16:43:44 +0900

    エージェント単体のparse完了

--- rcg_parser.ml
+++ rcg_parser.ml
@@ -14,30 +14,52 @@ end
 let parsep =
 "((l 1) 0 0x9 -49 0 0 0 2.327 0 (v h 180) (s 8000 1 1 130600) (c 0 0 906 0 1 907 1 0 0 0 0))";;
 
-type pos_x_y = X of int | Y of int;;
-
-type agent_l = Label of int;;
-
-type rcg =
-  Pos of (pos_x_y * pos_x_y)
+type agent =
+  Pos_x of int
+| Pos_y of int
 | Label of int
-| Etc of string
+;;
+
+type agent_inf =
+  Record of  (agent * agent * agent)
 ;;
 
 (* string -> t*)
 let lexing s = scan_sexp (Lexing.from_string s);;
 
-let parse_atom a =
-  function
+
+let matching a =
+  match a with
+    List ( List ( Atom "l" :: rest) :: (Atom a) ::  [List(Atom "d" :: rest2)])  -> a
+  | _ -> failwith "NO"
+;;
+
+let matching2 a =
+  match a with
+    List ( List ( Atom "l" :: [Atom num]) :: b1 :: b2
+           :: (Atom x) :: (Atom y) :: (Atom xv) :: (Atom yv)
+           :: b3 :: b4 :: rest2) ->
+      Record (Label (int_of_string num) ,
+              Pos_x (int_of_string x) , Pos_y (int_of_string y))
+  | _ -> failwith "NO"
+;;
+
+
+(* test *)
+(*matching (lexing parsep);;*)
+matching (lexing "((l a) a  (d e))");;
+matching2 (lexing parsep)
+
 
 (* string -> rcg *)
+(*
 let parse s =
   let rec parse s =
     match s with
       Atom a -> Atom (a)
     | List [] -> List []
     | List [Atom a] -> List [Atom a]
-    | List (Atom "l" :: :: rest) -> List (Atom a :: List.map parse rest)
+  (*  | List (Atom "l" :: :: rest) -> List (Atom a :: List.map parse rest) *)
     | List (Atom a :: rest) -> List (Atom a :: List.map parse rest)
     | List (List a :: rest)
       -> List (List (List .map parse a) :: List.map parse rest)
@@ -45,6 +67,20 @@ let parse s =
   let lex = Lexing.from_string s in
   parse (scan_sexp lex)
 ;;
+*)
+(*
+let parse s =
+  let parse s =
+    match s with
+      List (List (Atom a :: [Atom b]) :: rest)
+        -> (* Record(Label 1 , int_of_string b, int_of_string b) *)
+          ()
+    | _ -> failwith "Parse fail"
+  in
+  let lex = Lexing.from_string s in
+  parse (scan_sexp lex)
+;;
+*)
 
 (* string -> rcg *)
 (*
